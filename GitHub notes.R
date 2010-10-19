c514a854711512a00aaf048b5b9ba0a081323135
Nick Holschuh <holschun@carleton.edu>
2010-10-19 18:52:57 +0000

    Commenting added to the code

--- GitHub notes.R
+++ GitHub notes.R
@@ -1,9 +1,9 @@
 #########################################################
-#               Initial GitHub Setup                    #
+#            Initial GitHub Setup and Use               #
 #########################################################
 
 #These lines of code only need to be run once, and they need to be copied/pasted
-#into the console (RStudio has issues if you try and run them with the run command)
+#into the console (RStudio has issues if you try and do this with the run button)
 
 !git config --global user.name "Nick Holschuh"
 !git config --global user.email "holschun@carleton.edu"
@@ -22,7 +22,7 @@ file.edit("~/.ssh/id_rsa.pub")
 
 #Then the standard GitHub commands can be used to check out code,
 #but each command must be prefaced with an "!". For example, if your working
-#directory in RStudio is your repository folder the commands used would be:
+#directory in RStudio is your repository folder, the commands used would be:
 
 !git pull
 #for pulling down new files or changes to the repository
@@ -35,6 +35,8 @@ file.edit("~/.ssh/id_rsa.pub")
 
 !git commit -a --message "_____________"
 #to validate and comment on any changes to files previous to updating the repository
+#You must put the contents of your commit within quotes and after the message tag,
+#because RStudio is unable to open VI
 
 !git push
 #to push back all changes made after commit-ing
